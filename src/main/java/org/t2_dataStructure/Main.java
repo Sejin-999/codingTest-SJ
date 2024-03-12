50053b15a9af22abe991354ceccac98e50e96122
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2024-03-12 11:17:13 +0900

    arrSum

--- src/main/java/org/t2_dataStructure/Main.java
+++ src/main/java/org/t2_dataStructure/Main.java
@@ -7,30 +7,58 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    // 구간 합 구하기 - 11659
-    private static void sum_arr_1() throws IOException {
+    private static void sum_arr_3() throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int cnt = 0; //res 값
         int N = Integer.parseInt(stringTokenizer.nextToken());
         int M = Integer.parseInt(stringTokenizer.nextToken());
-        Long[] S = new Long[N + 1];
+        int arrN[] = new int[N];
+        int arrC[] = new int[M];
 
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        S[0] = 0L; // 초기값 설정
-        for (int i = 1; i <= N; i++) {
-            // 합배열 만들기
-            S[i] = S[i-1] + Integer.parseInt(stringTokenizer.nextToken());
-        }
-        for (int q = 0; q < M; q++) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int i = Integer.parseInt(stringTokenizer.nextToken());
-            int j = Integer.parseInt(stringTokenizer.nextToken());
-            System.out.println(S[j] - S[i - 1]);
+        for (int i = 0; i < N; i++) {
+            arrN[i] = Integer.parseInt(stringTokenizer.nextToken());
         }
 
+        //합배열 만들기
+        int arrS[] = new int[N+1];
+        arrS[0] = arrN[0];
+
+        for (int i = 1; i < N; i++) {
+            arrS[i] = arrN[i] + arrS[i - 1];
+        }
+
+        //나누어 떨어지는 경우 찾기
+        for (int i = 0; i < N; i++) {
+            int c = arrS[i] % M;
+
+            if (c == 0) {
+                cnt++;
+            }
+
+            arrC[c]++;
+
+        }
+        for (int i = 0; i < M; i++) {
+            if (arrC[i] > 1) {
+                //경우가 2개 이상인 경우만..
+                //경우의 수 구하는 공식..
+                /**
+                 * 이때 사용된 공식은 조합(combination)의 공식
+                 * 공식: nC2 = n! / (2! * (n-2)!)
+                 * 여기서 n!는 팩토리얼이며, n! = n * (n-1) * (n-2) * ... * 1 입니다.
+                 * 2!는 2 * 1로 계산됩니다. 따라서 nC2는 n(n-1)/2 가 됩니다
+                 */
+                cnt = cnt + (arrC[i] * (arrC[i] - 1) / 2);
+            }
+        }
+
+        System.out.println(cnt);
     }
+
     public static void main(String[] args) throws IOException {
-        sum_arr_1();
+        sum_arr_3();
     }
+
 }
--- src/main/java/org/t2_dataStructure/test1.java
+++ src/main/java/org/t2_dataStructure/test1.java
@@ -76,8 +76,145 @@ public class test1 {
         }
 
     }
+    //구간합 구하기 2 - 11660
+    private static void sum_arr_2() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int M = Integer.parseInt(stringTokenizer.nextToken());
+        int [][] arrN = new int[N+1][N+1]; // 원본 배열 ,arrNew
+        int [][] arrS = new int[N+1][N+1]; // 합 배열 , arrSum
+
+        //원본 배열 저장
+        for(int i=1; i<=N; i++){
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            for(int j=1; j<=N; j++){
+                arrN[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+            }
+        }
+        //합 배열 저장
+        /**
+         * 이전 행 , 이전 열 값 (+) 기존 값 (-) 이전행&이전열
+         */
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=N; j++){
+                arrS[i][j] = arrS[i-1][j]+arrS[i][j-1]+arrN[i][j]-arrS[i-1][j-1];
+            }
+        }
+        for(int i=0; i<M; i++){
+            //질의 받기
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int x1 = Integer.parseInt(stringTokenizer.nextToken());
+            int y1 = Integer.parseInt(stringTokenizer.nextToken());
+            int x2 = Integer.parseInt(stringTokenizer.nextToken());
+            int y2 = Integer.parseInt(stringTokenizer.nextToken());
+
+            int res = arrS[x2][y2] - arrS[x2][y1-1] - arrS[x1-1][y2] + arrS[x1-1][y1-1];
+
+            System.out.println(res);
+        }
+
+    }
+    //나머지 합 구하기 - 10986
+    private static void sum_arr_3() throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int cnt = 0; //res 값
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int M = Integer.parseInt(stringTokenizer.nextToken());
+        int arrN[] = new int[N];
+        int arrC[] = new int[M];
+
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for(int i=0; i<N;i++){
+            arrN[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        //합배열 만들기
+        int arrS[] = new int[N];
+        arrS[0] = arrN[0];
+
+        for(int i=1; i<N; i++){
+            arrS[i] = arrN[i]+arrS[i-1];
+        }
+
+        //나누어 떨어지는 경우 찾기
+        for(int i=0; i<N; i++){
+            int c = arrS[i] %M;
+
+            if(c==0){
+                cnt++;
+            }
+
+            arrC[c]++;
+
+        }
+        for(int i=0; i<M; i++){
+            if(arrC[i] > 1){
+                //경우가 2개 이상인 경우만..
+                //경우의 수 구하는 공식..
+                /**
+                 * 이때 사용된 공식은 조합(combination)의 공식
+                 * 공식: nC2 = n! / (2! * (n-2)!)
+                 * 여기서 n!는 팩토리얼이며, n! = n * (n-1) * (n-2) * ... * 1 입니다.
+                 * 2!는 2 * 1로 계산됩니다. 따라서 nC2는 n(n-1)/2 가 됩니다
+                 */
+                cnt = cnt + (arrC[i] *(arrC[i]-1) /2);
+            }
+        }
+
+        System.out.println(cnt);
+
+        //백준에 올린코드..
+        /**
+         import java.io.BufferedReader;
+         import java.io.IOException;
+         import java.io.InputStreamReader;
+         import java.util.StringTokenizer;
+
+         public class Main {
+         public static void main(String[] args) throws IOException {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+         // 입력 받기
+         StringTokenizer st = new StringTokenizer(br.readLine());
+         int N = Integer.parseInt(st.nextToken());
+         int M = Integer.parseInt(st.nextToken());
+
+         st = new StringTokenizer(br.readLine());
+         int[] arr = new int[N];
+         for (int i = 0; i < N; i++) {
+         arr[i] = Integer.parseInt(st.nextToken());
+         }
+
+         // 부분 합 배열 만들기
+         long[] prefixSum = new long[N + 1];
+         for (int i = 1; i <= N; i++) {
+         prefixSum[i] = (prefixSum[i - 1] + arr[i - 1]) % M;
+         }
+
+         // 나머지가 같은 부분 합의 개수 세기
+         long[] count = new long[M];
+         for (int i = 0; i <= N; i++) {
+         count[(int) prefixSum[i]]++;
+         }
+
+         // 나머지가 같은 부분 합에서 2개를 선택하는 경우의 수 계산
+         long result = 0;
+         for (int i = 0; i < M; i++) {
+         result += (count[i] * (count[i] - 1)) / 2;
+         }
+
+         System.out.println(result);
+         }
+         }
+
+         */
+    }
+
     public static void main(String[] args) throws IOException {
-        sum_arr_1();
+        sum_arr_3();
     }
 
 
