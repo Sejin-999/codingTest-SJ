7e2102be7084e8394e91759c7d3536342c07a4a5
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2024-03-14 12:30:00 +0900

    stack & Queue

--- src/main/java/org/t2_dataStructure/Main.java
+++ src/main/java/org/t2_dataStructure/Main.java
@@ -4,25 +4,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static void stack_03(){
-        Queue<Integer> queue = new LinkedList<>();
+    private static void queue_02(){
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a1,a2) ->{
+            int first_abs = Math.abs(a1); //absolute value , 절대값이라는 뜻임
+            int second_abs = Math.abs(a2);
+
+            if(first_abs == second_abs){
+                return a1> a2 ? 1: -1;
+            }else{
+                return first_abs-second_abs;
+                /**
+                 * 절대값기준
+                 * a1 - a2 일때 양수가 나오면 a1이 a2보다 크다는 것임 -> 즉 a2가 먼저 우선순위에 올라가고 그 이후 a1임
+                 * a1 - a2 일때 음수가 나오면 a2가 a1보다 크다는 것임 -> 즉 a1이 먼저 우선순위에 올라가고 그 이후 a2가 올라감
+                 * 위 비교연산자도 동일한 원리
+                 */
+            }
+        });
+
         Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
 
-        int n = scan.nextInt();
+        for(int i=1; i<=N; i++){
+            int x = scan.nextInt();
 
-        for(int i = 1; i <=n; i++){
-            queue.add(i);
+            /**
+             * todo
+             * x == 0 -> poll 하기 , 만약 비어있는경우 0을 출력
+             * x != 0 -> add 하기
+             */
+
+            if(x ==0){
+                if(queue.isEmpty()){
+                    System.out.println("0");
+                }else{
+                    System.out.println(queue.poll());
+                }
+            }else{
+                queue.add(x);
+            }
         }
 
-        while (queue.size() > 1){
-            queue.poll();
-            queue.add(queue.poll());
-        }
-        //size 1
-        System.out.println(queue.peek());
+
     }
+
     public static void main(String[] args) throws IOException {
-        stack_03();
+        queue_02();
     }
-
 }
--- src/main/java/org/t2_dataStructure/test4.java
+++ src/main/java/org/t2_dataStructure/test4.java
@@ -101,7 +101,7 @@ public class test4 {
         bw.flush();
     }
     //card game / 2164
-    private static void stack_03(){
+    private static void queue_01(){
         Queue<Integer> queue = new LinkedList<>();
         Scanner scan = new Scanner(System.in);
 
@@ -118,8 +118,53 @@ public class test4 {
         //size 1
         System.out.println(queue.peek());
     }
+
+    private static void queue_02(){
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a1,a2) ->{
+            int first_abs = Math.abs(a1); //absolute value , 절대값이라는 뜻임
+            int second_abs = Math.abs(a2);
+
+            if(first_abs == second_abs){
+                return a1> a2 ? 1: -1;
+            }else{
+                return first_abs-second_abs;
+                /**
+                 * 절대값기준
+                 * a1 - a2 일때 양수가 나오면 a1이 a2보다 크다는 것임 -> 즉 a2가 먼저 우선순위에 올라가고 그 이후 a1임
+                 * a1 - a2 일때 음수가 나오면 a2가 a1보다 크다는 것임 -> 즉 a1이 먼저 우선순위에 올라가고 그 이후 a2가 올라감
+                 * 위 비교연산자도 동일한 원리
+                 */
+            }
+        });
+
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+
+        for(int i=1; i<=N; i++){
+            int x = scan.nextInt();
+
+            /**
+             * todo
+             * x == 0 -> poll 하기 , 만약 비어있는경우 0을 출력
+             * x != 0 -> add 하기
+             */
+
+            if(x ==0){
+                if(queue.isEmpty()){
+                    System.out.println("0");
+                }else{
+                    System.out.println(queue.poll());
+                }
+            }else{
+                queue.add(x);
+            }
+        }
+
+
+    }
+
     public static void main(String[] args) throws IOException {
-        stack_03();
+        queue_02();
     }
 
 }
